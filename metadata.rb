ea9feb6501a679a3dde9fdf5371f26c817ebc13b
Cameron Johnston <cameron@needle.com>
2011-06-16 13:45:04 -0600

    initial commit of cookbook[celery]

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Needle Inc."
+maintainer_email "cameron@needle.com"
+license          "Apache 2.0"
+description      "Installs/Configures components of the celery distributed task queue system"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+supports        'debian'
+supports        'ubuntu'
+depends         "python"
---
+++ recipes/default.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: celery
+# Recipe:: default
+#
+# Copyright 2011, Needle Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "python"
+
+if node[:celery][:virtualenv]
+  python_virtualenv node[:celery][:virtualenv]
+end
+
+python_pip "celery" do
+  if node[:celery][:virtualenv]
+    virtualenv node[:celery][:virtualenv]
+  end
+end
