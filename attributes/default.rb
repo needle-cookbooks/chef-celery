c9b778ec8df19b54d679930f533822a8e0f47384
Cameron Johnston <cameron@needle.com>
2011-06-16 14:29:18 -0600

    fixing attributes syntax in cookbook[celery]

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,19 +1,19 @@
-default celery[:virtualenv] = nil
+default[:celery][:virtualenv] = nil
 # celeryd specific attributes
-default celeryd[:pid_file] = "/var/run/celeryd.pid"
-default celeryd[:log_file] = "/var/log/celeryd.log"
-default celeryd[:log_level] = "INFO"
-default celeryd[:user] = nil
-default celeryd[:group] = nil
-default celeryd[:executable] = "celeryd" # FIXME hope it's in your path!
-default celeryd[:chdir] = nil
-default celeryd[:opts] =  nil
+default[:celeryd][:pid_file] = "/var/run/celeryd.pid"
+default[:celeryd][:log_file] = "/var/log/celeryd.log"
+default[:celeryd][:log_level] = "INFO"
+default[:celeryd][:user] = nil
+default[:celeryd][:group] = nil
+default[:celeryd][:executable] = "celeryd" # FIXME hope it's in your path!
+default[:celeryd][:chdir] = nil
+default[:celeryd][:opts] =  nil
 # celerybeat specific attributes
-default celerybeat[:pid_file] = "/var/run/celerybeat.pid"
-default celerybeat[:log_file] = "/var/log/celerybeat.log"
-default celerybeat[:log_level] = "INFO"
-default celerybeat[:user] = nil
-default celerybeat[:group] = nil
-default celerybeat[:executable] = "celerybeat" # FIXME hope it's in your path!
-default celerybeat[:chdir] = nil
-default celerybeat[:opts] =  nil
+default[:celerybeat][:pid_file] = "/var/run/celerybeat.pid"
+default[:celerybeat][:log_file] = "/var/log/celerybeat.log"
+default[:celerybeat][:log_level] = "INFO"
+default[:celerybeat][:user] = nil
+default[:celerybeat][:group] = nil
+default[:celerybeat][:executable] = "celerybeat" # FIXME hope it's in your path!
+default[:celerybeat][:chdir] = nil
+default[:celerybeat][:opts] =  nil
