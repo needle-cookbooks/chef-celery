b5c66bc8d97f11bcbb2b78d89f9a4113ab671830
Cameron Johnston <cameron@needle.com>
2011-07-12 17:15:23 -0600

    refactor cookbook[celery] for version 0.0.2 which now uses more generic init scripts instead of start-stop-daemon scripts

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cameron@needle.com"
 license          "Apache 2.0"
 description      "Installs/Configures components of the celery distributed task queue system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
 supports        'debian'
 supports        'ubuntu'
 depends         "python"
