2fd32367cb5792b5af951dfd02dc9f62419f0bc5
Cameron Johnston <cameron@needle.com>
2011-07-26 10:49:08 -0600

    adding logrotate config to cookbook[celery]

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,10 @@
 default[:celery][:virtualenv] = nil
 default[:celery][:django] = false
 default[:celery][:config_module] = nil
+default[:celery][:logdir] = "/var/log/celery"
 # celeryd specific attributes
 default[:celery][:celeryd_pid_file] = "/var/run/celeryd.pid"
-default[:celery][:celeryd_log_file] = "/var/log/celeryd.log"
+default[:celery][:celeryd_log_file] = "#{node[:celery][:logdir]}/celeryd.log"
 default[:celery][:celeryd_log_level] = "INFO"
 default[:celery][:celeryd_user] = nil
 default[:celery][:celeryd_group] = nil
@@ -14,7 +15,7 @@ default[:celery][:celeryd_chdir] = nil
 default[:celery][:celeryd_opts] =  nil
 # celerybeat specific attributes
 default[:celery][:celerybeat_pid_file] = "/var/run/celerybeat.pid"
-default[:celery][:celerybeat_log_file] = "/var/log/celerybeat.log"
+default[:celery][:celerybeat_log_file] = "#{node[:celery][:logdir]}/celerybeat.log"
 default[:celery][:celerybeat_log_level] = "INFO"
 default[:celery][:celerybeat_user] = nil
 default[:celery][:celerybeat_group] = nil
