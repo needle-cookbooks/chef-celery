4220ed110e7fdd5392d9525e7fb40179cffa7747
Cameron Johnston <cameron@needle.com>
2011-10-17 17:55:29 -0600

    adding amqp specific attributes to cookbook[celery]

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,6 +14,14 @@ default[:celery][:celeryd_multi_cmd] = "celeryd_multi"
 default[:celery][:celeryctl_cmd] = "celeryctl"
 default[:celery][:celeryd_chdir] = nil
 default[:celery][:celeryd_opts] =  nil
+# amqp specific attributes
+default[:celery][:celeryd_use_amqp] = true
+default[:celery][:amqp_task_result_expires] = 300
+default[:celery][:amqp_task_result_connection_max] = 1
+default[:celery][:amqp_result_exchange] = "celeryresults"
+default[:celery][:amqp_result_exchange_type] = "direct"
+default[:celery][:amqp_result_serializer] = "pickle"
+default[:celery][:amqp_result_persistent] = false
 # celerybeat specific attributes
 default[:celery][:celerybeat_pid_file] = "/var/run/celerybeat.pid"
 default[:celery][:celerybeat_log_file] = "#{node[:celery][:logdir]}/celerybeat.log"
