368ac990dd2cac389ed754120bb9a2deab346f2c
Cameron Johnston <cameron@needle.com>
2012-04-25 11:19:10 -0600

    fixing syntax error in celerycam service block

--- recipes/celerycam.rb
+++ recipes/celerycam.rb
@@ -3,7 +3,7 @@ include_recipe 'celery::default'
 
 runit_service 'celerycam'
 
-service "celerycam"
+service "celerycam" do
   stop_command "sv stop celerycam"
   start_command "sv start celerycam"
 end
