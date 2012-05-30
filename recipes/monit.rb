01916ee8e8b793986d2350fee9ef2a27bb895a20
Cameron Johnston <cameron@needle.com>
2012-05-30 13:59:14 -0600

    adding monit recipe and associated template files

---
+++ recipes/monit.rb
@@ -0,0 +1,22 @@
+include_recipe 'monit'
+
+if node['recipes'].include?('celery::worker') or node['recipes'].include?('celery::celeryd')
+  monitrc 'celeryd' do
+    source 'monit-celeryd.conf.erb'
+    cookbook 'celery'
+  end
+end
+
+if node['recipes'].include?('celery::scheduler') or node['recipes'].include?('celery::celerybeat')
+  monitrc 'celerybeat' do
+    source 'monit-celerybeat.conf.erb'
+    cookbook 'celery'
+  end
+end
+
+if node['recipes'].include?('celery::celerycam')
+  monitrc 'celerycam' do
+    source 'monit-celerycam.conf.erb'
+    cookbook 'celery'
+  end
+end
