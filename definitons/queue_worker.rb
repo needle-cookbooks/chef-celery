21ed35a431a8fd12fc261a3208fb4a4708bdc77e
Cameron Johnston <cameron@needle.com>
2012-08-05 19:56:00 -0600

    fixing missing parens in queue_worker definition

--- definitons/queue_worker.rb
+++ definitons/queue_worker.rb
@@ -7,7 +7,7 @@ define :celeryd_node
 	template "/etc/defaults/celeryd-#{params['name']}" do
 		source 'celeryd-defaults.erb'
 		mode 0700
-		variables { :options => params[:options] }
+		variables({ :options => params[:options] })
 	end
 
 end
