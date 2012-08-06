c71fc456aa09bdfa385d5fd404866544a7846765
Cameron Johnston <cameron@needle.com>
2012-08-05 19:24:55 -0600

    bumping cookbook version to 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cameron@needle.com"
 license          "Apache 2.0"
 description      "Installs/Configures components of the celery distributed task queue system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.4"
+version          "0.1.0"
 supports        'debian'
 supports        'ubuntu'
 depends         'python'
