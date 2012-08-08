d8fee28d78222371ea3e7dc8f1f9d8c5bf171f8d
Cameron Johnston <cameron@needle.com>
2012-08-08 13:27:30 -0600

    removing recipes made superfluous by the new celery process definitions

--- recipes/celerycam.rb
+++
@@ -1,9 +0,0 @@
-include_recipe 'runit'
-include_recipe 'celery::default'
-
-runit_service 'celerycam'
-
-service "celerycam" do
-  stop_command "sv stop celerycam"
-  start_command "sv start celerycam"
-end
