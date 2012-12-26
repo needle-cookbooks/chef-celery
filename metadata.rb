bb8c6d97f340f93a0d332fbe7cf440dcc43342db
Cameron Johnston <cameron@needle.com>
2012-12-26 12:00:13 -0700

    bump to 0.1.3

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "cookbooks@needle.com"
 license          "Apache 2.0"
 description      "Installs/Configures components of the celery distributed task queue system"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version         '0.1.2'
+version         '0.1.3'
 supports        'debian'
 supports        'ubuntu'
 depends         'python'
